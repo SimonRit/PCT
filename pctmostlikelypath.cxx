a92f442e5b1add4d8140e2127e1738de0962d6e3
Simon Rit <simon.rit@creatis.insa-lyon.fr>
2012-02-01 19:39:22 +0100

    Same as itkProtonPairsToDistanceDrivenProjection.txx

--- pctmostlikelypath.cxx
+++ pctmostlikelypath.cxx
@@ -66,9 +66,11 @@ int main(int argc, char * argv[])
   if(quadricIn->Evaluate(dIn) && quadricOut->Evaluate(dOut))
     {
     pSIn  = pIn  + dIn  * quadricIn ->GetNearestDistance();
-    if(pSIn[2]<pIn[2])
+    if(pSIn[2]<pIn[2]  || pSIn[2]>pOut[2])
       pSIn  = pIn  + dIn  * quadricIn ->GetFarthestDistance();
-    pSOut = pOut + dOut * quadricOut->GetFarthestDistance();
+    pSOut = pOut + dOut * quadricOut->GetNearestDistance();
+    if(pSOut[2]<pIn[2] || pSOut[2]>pOut[2])
+      pSOut = pOut + dOut * quadricOut->GetFarthestDistance();
     }
   OutputImageType::IndexType index;
   index[0] = 0;
