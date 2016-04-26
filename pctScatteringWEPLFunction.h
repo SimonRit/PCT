15899af38e733b5cab2a4873ab0e92337177a02d
catherine quinones <quinones@creatis.insa-lyon.fr>
2016-04-26 18:16:09 +0200

    Addded classes to calculate the sigma2 of either highland's or the differential moliere's scattering angle

--- pctScatteringWEPLFunction.h
+++ pctScatteringWEPLFunction.h
@@ -5,10 +5,6 @@
 #include "CLHEP/Units/PhysicalConstants.h"
 #include "TMath.h"
 
-#include <fstream>
-#include <iostream>
-#include <iomanip>
-
 namespace pct
 {
 
@@ -17,50 +13,59 @@ namespace Functor
 
 namespace ScatteringWEPL
 {
-
 std::vector<double> sigma2_lut;
 std::vector<int> thickness_lut;
 unsigned int range = 0;
+const double proton_mass_c2 = CLHEP::proton_mass_c2;
+
+// Equation 8 of Schulte Med Phys 2008
+class Highland
+{
+public:
+  static double GetSigma2(const double thickness, const double invbeta2p2)
+    {
+    const double E0 = 13.6*CLHEP::MeV;
+    const double X0 = 38.08*CLHEP::cm;
+    return E0*E0/X0 * (1+0.038*vcl_log(thickness/X0))* (1+0.038*vcl_log(thickness/X0)) * invbeta2p2;
+    }
+};
+
+// Equations 39 and 40 of Gottschalk Med Phys 2010
+class DifferentialMoliere
+{
+public:
+  static double GetSigma2(const double eIn, const double invbeta2p2)
+    {
+    const double p1v1 = 1./((eIn*CLHEP::MeV + proton_mass_c2) / (eIn*CLHEP::MeV + 2*proton_mass_c2) / eIn*CLHEP::MeV);
+    const double Xs = 46.88*CLHEP::cm; 
+    const double pv = TMath::Sqrt(1./invbeta2p2);
+    double fdm = (0.5244 + 0.1975 * TMath::Log10(1-(pv/p1v1)*(pv/p1v1)) + 0.2320 * TMath::Log10(pv) - 0.0098 * TMath::Log10(pv) * TMath::Log10(1-(pv/p1v1)*(pv/p1v1)));
+    return fdm * 15.* 15. / pv / pv / Xs;
+    }
+};
 
 class ScatteringLUT
 {
 public:
+  // LUT from analytical scattering models   
   static double SetLUT(const double eIn)
     {
     double m_IonizationPotential = 68.9984*CLHEP::eV; 
     pct::Functor::IntegratedBetheBlochProtonStoppingPowerInverse<float, double> bethe( m_IonizationPotential, 500 * CLHEP::MeV );
-
     unsigned const int max_waterthickness = 500*CLHEP::mm;
-    //const double E0 = 13.6*CLHEP::MeV;
-    //const double X0 = 38.08*CLHEP::cm;
-    const double proton_mass_c2 = CLHEP::proton_mass_c2;
-    //double integratedinvbeta2p2 = 0.;
-    //double invbeta2p2  = 0.;
+    double invbeta2p2  = 0.;
     double energycurrent  = 0.;
     double sigma2 = 0.;
 
-    const double p1v1 = 1./((eIn*CLHEP::MeV + proton_mass_c2) / (eIn*CLHEP::MeV + 2*proton_mass_c2) / eIn*CLHEP::MeV);
-    const double Xs = 46.88*CLHEP::cm; 
-    double pv = 0.;
-    double fdm = 0.;
- 
     for(unsigned int waterthickness = 1; waterthickness<max_waterthickness; waterthickness++)
       {    
       energycurrent = bethe.GetEnergy(waterthickness*CLHEP::mm, eIn*CLHEP::MeV);
       if(energycurrent<0.001) break;        
-      //invbeta2p2 = (energycurrent+proton_mass_c2)*(energycurrent+proton_mass_c2) / 
-      //            ((energycurrent+2*proton_mass_c2)*(energycurrent+2*proton_mass_c2) * energycurrent*energycurrent); 
-      //integratedinvbeta2p2 += invbeta2p2;
-      //sigma2 = E0*E0/X0 * (1+0.038*vcl_log(waterthickness/X0))* (1+0.038*vcl_log(waterthickness/X0)) * integratedinvbeta2p2; 
-      //sigma2 = E0*E0/X0 * integratedinvbeta2p2; 
-
-      //__________Differential Moliere
-      pv = ((energycurrent + 2*proton_mass_c2) * energycurrent) / (energycurrent + proton_mass_c2);
-      fdm = (0.5244 + 0.1975 * TMath::Log10(1-(pv/p1v1)*(pv/p1v1)) + 0.2320 * TMath::Log10(pv) - 0.0098 * TMath::Log10(pv) * TMath::Log10(1-(pv/p1v1)*(pv/p1v1)));
-      sigma2 += fdm * 15.* 15. / pv / pv / Xs;
-      //___________________________________
-
-      //std::cout<<waterthickness<<"\t"<< sigma2 <<std::endl;   
+      invbeta2p2 = (energycurrent+proton_mass_c2)*(energycurrent+proton_mass_c2) / 
+                  ((energycurrent+2*proton_mass_c2)*(energycurrent+2*proton_mass_c2) * energycurrent*energycurrent); 
+      //sigma2 += Highland::GetSigma2(waterthickness,invbeta2p2); 
+      sigma2 += DifferentialMoliere::GetSigma2(eIn,invbeta2p2); // Closer to the G4 values than Highland's 
+      //std::cout<< sigma2 <<std::endl;   
       sigma2_lut.push_back( sigma2 );
       thickness_lut.push_back(waterthickness);
       range = waterthickness; 
