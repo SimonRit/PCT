SimonRit/PCT

commit 3e95979a64e450f2a33c972fcc0e3401a6773bfa

added option to write WET instead of entrance energy

pctpaircuts.cxx
@@ -5,6 +5,10 @@
 #include <rtkConstantImageSource.h>
 
 #include "pctProtonPairsToDistanceDrivenProjection.h"
+#include "pctThirdOrderPolynomialMLPFunction.h"
+#include "pctSchulteMLPFunction.h"
+#include "pctEnergyStragglingFunctor.h"
+#include "pctBetheBlochFunctor.h"
 
 #include <itkImageFileWriter.h>
 #include <itkRegularExpressionSeriesFileNames.h>
@@ -51,6 +55,9 @@ int main(int argc, char * argv[])
   const size_t npixels = sumEnergy->GetOutput()->GetBufferedRegion().GetNumberOfPixels();
   std::vector< std::vector<double> > energies(npixels);
   std::vector< std::vector<double> > angles(npixels);
+
+  pct::Functor::IntegratedBetheBlochProtonStoppingPowerInverse<float, double> *ConvFunc;
+  ConvFunc = new pct::Functor::IntegratedBetheBlochProtonStoppingPowerInverse<float, double>(68.9984, 600.*CLHEP::MeV, 0.1*CLHEP::keV);
 
   // Read pairs
   typedef itk::Vector<float, 3> VectorType;
@@ -305,6 +312,17 @@ int main(int argc, char * argv[])
       const double angley = vcl_acos( std::min(1.,dInY*dOutY / ( dInY.GetNorm() * dOutY.GetNorm() ) ) );
       const double energy = (data[0]==0.)?data[1]:data[0]-data[1];
 
+      VectorType WET_data;
+      if(args_info.wet_flag)
+       {
+        WET_data[0]=ConvFunc->GetValue(data[1], data[0]);
+        }
+      else
+        WET_data[0]=data[0];
+
+      WET_data[1]=data[1];
+      WET_data[2]=data[2];
+
       if( anglex <= pSumAngleSq [idx] &&
           angley <= pSumAngleSq [idx] &&
           vcl_abs(energy-pSumEnergy[idx]) <= pSumEnergySq[idx] )
@@ -315,7 +333,7 @@ int main(int argc, char * argv[])
           pairs.push_back(pOut);
           pairs.push_back(dIn);
           pairs.push_back(dOut);
-          pairs.push_back(data);
+          pairs.push_back(WET_data);
           if(region.GetSize(0)==6)
             pairs.push_back(nuclearinfo);
           }
@@ -325,7 +343,7 @@ int main(int argc, char * argv[])
           pairs.push_back(pOut);
           pairs.push_back(dIn);
           pairs.push_back(dOut);
-          pairs.push_back(data);
+          pairs.push_back(WET_data);
           if(region.GetSize(0)==6)
             pairs.push_back(nuclearinfo);
           }
@@ -335,7 +353,7 @@ int main(int argc, char * argv[])
           pairs.push_back(pOut);
           pairs.push_back(dIn);
           pairs.push_back(dOut);
-          pairs.push_back(data);
+          pairs.push_back(WET_data);
           if(region.GetSize(0)==6)
             pairs.push_back(nuclearinfo);
           }

pctpaircuts.ggo
@@ -13,6 +13,7 @@ option "robustopt" - "Use newer options for robust cut."            int       no
 option "plotpix"   p "Pixel index of binning for output plot"       int       no
 option "primaries" - "Consider only primary protons"                flag      off
 option "nonuclear" - "Consider only primary protons without nuclear interactions"                flag      off
+option "wet" - "Write WET instead of initial energy"                flag      off
 
 section "Projections parameters"
 option "origin"    - "Origin (default=centered)" double multiple no

commit 2d239acebae1d1ad4fbf11f2186040cc03bb5287

FIX: ionization potential lacked unit for Bethe Bloch in pctpaircuts
The bug only shows up without Geant4 which is why it was not caught up.
The bug was introduced by 3e95979.

pctpaircuts.cxx
@@ -57,7 +57,7 @@ int main(int argc, char * argv[])
   std::vector< std::vector<double> > angles(npixels);
 
   pct::Functor::IntegratedBetheBlochProtonStoppingPowerInverse<float, double> *ConvFunc;
-  ConvFunc = new pct::Functor::IntegratedBetheBlochProtonStoppingPowerInverse<float, double>(68.9984, 600.*CLHEP::MeV, 0.1*CLHEP::keV);
+  ConvFunc = new pct::Functor::IntegratedBetheBlochProtonStoppingPowerInverse<float, double>(68.9984*CLHEP::eV, 600.*CLHEP::MeV, 0.1*CLHEP::keV);
 
   // Read pairs
   typedef itk::Vector<float, 3> VectorType;