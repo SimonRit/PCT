8c1eb7709f05ddef3e8a25fab8b851cdf1b1ff66
Simon Rit <simon.rit@creatis.insa-lyon.fr>
2022-01-25 07:23:25 +0100

    STYLE: remove debug code

--- pctpairprotonsLMU_IMPCT.cxx
+++ pctpairprotonsLMU_IMPCT.cxx
@@ -14,8 +14,6 @@
 
 #define MAX_RUNS 4096
 
-#define WEPLRECOHACK 0
-
 struct ParticleData
   {
   itk::Vector<float,3> position;
@@ -90,20 +88,11 @@ void WritePairs(const std::vector< std::pair<ParticleData,ParticleData> > &pairs
 
   itk::ImageRegionIterator<ImageType> it(img, region);
   PixelType eet;
-#if WEPLRECOHACK
-  std::cerr << "WARNING: Performing reconstruction of ideal WEPL!" << std::endl;
-#endif
   for(size_t i=0; i<pairs.size(); i++)
     {
-#if WEPLRECOHACK
-    eet[0] = 0.;
-    eet[1] = pairs[i].second.wepl - pairs[i].first.wepl;
-    eet[2] = 0.;//pairs[i].second.time - pairs[i].first.time;
-#else
     eet[0] = pairs[i].first.ekine;
     eet[1] = pairs[i].second.ekine;
     eet[2] = 0.;//pairs[i].second.time - pairs[i].first.time;
-#endif
 
     it.Set( pairs[i].first.position );
     ++it;
@@ -134,21 +123,6 @@ void WritePairs(const std::vector< std::pair<ParticleData,ParticleData> > &pairs
 int main(int argc, char * argv[])
 {
   GGO(pctpairprotonsLMU_IMPCT, args_info); //RTK macro parsing options from .ggo file (rtkMacro.h)
-
-#if WEPLRECOHACK
-  std::cout << "WARNING WARNING WARNING WARNING" << std::endl;
-  std::cout << "Reads WEPL instead of energy information!!" << std::endl;
-  std::cout << "WARNING WARNING WARNING WARNING" << std::endl;
-  std::cout << "WARNING WARNING WARNING WARNING" << std::endl;
-  std::cout << "Reads WEPL instead of energy information!!" << std::endl;
-  std::cout << "WARNING WARNING WARNING WARNING" << std::endl;
-  std::cout << "WARNING WARNING WARNING WARNING" << std::endl;
-  std::cout << "Reads WEPL instead of energy information!!" << std::endl;
-  std::cout << "WARNING WARNING WARNING WARNING" << std::endl;
-  std::cout << "WARNING WARNING WARNING WARNING" << std::endl;
-  std::cout << "Reads WEPL instead of energy information!!" << std::endl;
-  std::cout << "WARNING WARNING WARNING WARNING" << std::endl;
-#endif
 
   // Create root trees
   TChain *treeIn = new TChain("PhaseSpaceTree");
