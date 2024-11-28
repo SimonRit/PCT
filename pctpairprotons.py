44ecbcf989b1ff52500adfdbed24572da604503e
Aurélien Coussat <aurelien.coussat@creatis.insa-lyon.fr>
2024-11-28 15:25:30 +0100

    ENH: remove dependency on pandas

--- pctpairprotons.py
+++ pctpairprotons.py
@@ -4,7 +4,7 @@ import uproot
 import itk
 
 import numpy as np
-import pandas as pd
+import numpy.lib.recfunctions as rfn
 
 def main():
 
@@ -34,8 +34,8 @@ def main():
             pass
 
     def load_tree_as_df(root_file, tree_name):
-        tree = uproot.open(root_file)[tree_name]
-        df = tree.arrays([
+
+        branch_names = [
             'RunID',
             'EventID',
             'TrackID',
@@ -47,43 +47,73 @@ def main():
             'Direction_X',
             'Direction_Y',
             'Direction_Z'
-        ], library='pd')
-        df.rename(columns={
+        ]
+
+        tree = uproot.open(root_file)[tree_name]
+        branches = tree.arrays(branch_names, library='np')
+
+        dtype = [(name, branch.dtype) for name, branch in branches.items()]
+        ps = np.rec.recarray((len(branches['RunID']), ), dtype=dtype)
+        for branch_name in branch_names:
+            ps[branch_name] = branches[branch_name]
+
+        ps = rfn.rename_fields(ps, {
             'Position_' + str(args_info.proju): 'u',
             'Position_' + str(args_info.projv): 'v',
             'Position_' + str(args_info.projw): 'w',
-        }, inplace=True)
-        df.rename(columns={
+        })
+        ps = rfn.rename_fields(ps, {
             'Direction_' + str(args_info.proju): 'du',
             'Direction_' + str(args_info.projv): 'dv',
             'Direction_' + str(args_info.projw): 'dw',
-        }, inplace=True)
+        })
 
-        df = df[(df['RunID'] >= args_info.min_run) & (df['RunID'] < args_info.max_run)]
+        ps = ps[(ps['RunID'] >= args_info.min_run) & (ps['RunID'] < args_info.max_run)]
 
-        return df
+        return ps
 
-    df_in = load_tree_as_df(args_info.input_in, 'PhaseSpaceIn')
-    df_in['w'] = args_info.plane_in
-    verbose("Read input phase space:\n" + str(df_in))
+    ps_in = load_tree_as_df(args_info.input_in, 'PhaseSpaceIn')
+    ps_in['w'] = args_info.plane_in
+    verbose("Read input phase space:\n" + str(ps_in))
 
-    df_out = load_tree_as_df(args_info.input_out, 'PhaseSpaceOut')
-    df_out['w'] = args_info.plane_out
-    verbose("Read output phase space:\n" + str(df_out))
+    ps_out = load_tree_as_df(args_info.input_out, 'PhaseSpaceOut')
+    ps_out['w'] = args_info.plane_out
+    verbose("Read output phase space:\n" + str(ps_out))
 
     merge_columns = ['RunID', 'EventID']
     if args_info.no_nuclear:
         merge_columns.append('TrackID')
-    df_pairs = pd.merge(
-        df_in.drop_duplicates(merge_columns),  # drop_duplicates to mimic the behavior of C++ pctpairprotons (see comment line 272, commit 8260d3)
-        df_out,
-        on=merge_columns,
-        how='inner',
-        suffixes=('_in', '_out')
-    )
-    verbose("Merged input and output phase spaces:\n" + str(df_pairs))
+    
+    # Remove duplicates
+    _, unique_index = np.unique(ps_in[merge_columns], return_index=True)
+    ps_in = ps_in[unique_index]
 
-    number_of_runs = df_pairs['RunID'].max() + 1
+    ps_in.dtype.names = [n if n in merge_columns else n + '_in' for n in ps_in.dtype.names]
+    ps_out.dtype.names = [n if n in merge_columns else n + '_out' for n in ps_out.dtype.names]
+    ps_in_uniques = [n for n in ps_in.dtype.names if n not in merge_columns]
+    ps_out_uniques = [n for n in ps_out.dtype.names if n not in merge_columns]
+
+    if args_info.no_nuclear:
+        # Easy case, there should be at most one row in ps_in and ps_out for keys ['RunID', 'EventID', 'TrackID']
+        intersect, intersect_in, intersect_out = np.intersect1d(ps_in[merge_columns], ps_out[merge_columns], return_indices=True)
+        pairs = rfn.merge_arrays((intersect, ps_in[ps_in_uniques][intersect_in], ps_out[ps_out_uniques][intersect_out]), asrecarray=True, flatten=True)
+    else:
+        # More complicated, there can be more than one row in ps_out for keys ['RunID', 'EventID'] (because of 'TrackID')
+        # The solution is to repeat the computation for many TrackIDs, then merge it all together
+        track_max = ps_out['TrackID_out'].max()
+        verbose("Identified maximum number of tracks: " + str(track_max))
+        pairs_list = []
+        for t in range(track_max + 1):
+            ps_out_t = ps_out[ps_out['TrackID_out'] == t]
+            intersect, intersect_in, intersect_out = np.intersect1d(ps_in[merge_columns], ps_out_t[merge_columns], return_indices=True)
+            pairs = rfn.merge_arrays((intersect, ps_in[ps_in_uniques][intersect_in], ps_out_t[ps_out_uniques][intersect_out]), asrecarray=True, flatten=True)
+            if len(pairs) > 0:
+                pairs_list.append(pairs)
+        pairs = rfn.stack_arrays(pairs_list, asrecarray=True)
+        np.recarray.sort(pairs, order=['RunID', 'EventID', 'TrackID_in', 'TrackID_out'])
+    verbose("Merged input and output phase spaces.")
+
+    number_of_runs = pairs['RunID'].max() + 1
     verbose("Identified number of runs: " + str(number_of_runs))
 
     ComponentType = itk.ctype('float')
@@ -92,28 +122,28 @@ def main():
 
     run_range = range(args_info.min_run, min(number_of_runs, args_info.max_run))
     for r in run_range:
-        df_run = df_pairs[df_pairs['RunID'] == r]
-        if len(df_run) == 0:
+        ps_run = pairs[pairs['RunID'] == r]
+        if len(ps_run) == 0:
             continue
 
-        df_np = np.empty(shape=(len(df_run), 5, 3), dtype=np.float32)
-        df_np[:,0,0] = df_run['u_in']
-        df_np[:,0,1] = df_run['v_in']
-        df_np[:,0,2] = df_run['w_in']
-        df_np[:,1,0] = df_run['u_out']
-        df_np[:,1,1] = df_run['v_out']
-        df_np[:,1,2] = df_run['w_out']
-        df_np[:,2,0] = df_run['du_in']
-        df_np[:,2,1] = df_run['dv_in']
-        df_np[:,2,2] = df_run['dw_in'] * args_info.wweight
-        df_np[:,3,0] = df_run['du_out']
-        df_np[:,3,1] = df_run['dv_out']
-        df_np[:,3,2] = df_run['dw_out'] * args_info.wweight
-        df_np[:,4,0] = df_run['KineticEnergy_in']
-        df_np[:,4,1] = df_run['KineticEnergy_out']
-        df_np[:,4,2] = df_run['TrackID'] if args_info.no_nuclear else df_run['TrackID_out']
+        ps_np = np.empty(shape=(len(ps_run), 5, 3), dtype=np.float32)
+        ps_np[:,0,0] = ps_run['u_in']
+        ps_np[:,0,1] = ps_run['v_in']
+        ps_np[:,0,2] = ps_run['w_in']
+        ps_np[:,1,0] = ps_run['u_out']
+        ps_np[:,1,1] = ps_run['v_out']
+        ps_np[:,1,2] = ps_run['w_out']
+        ps_np[:,2,0] = ps_run['du_in']
+        ps_np[:,2,1] = ps_run['dv_in']
+        ps_np[:,2,2] = ps_run['dw_in'] * args_info.wweight
+        ps_np[:,3,0] = ps_run['du_out']
+        ps_np[:,3,1] = ps_run['dv_out']
+        ps_np[:,3,2] = ps_run['dw_out'] * args_info.wweight
+        ps_np[:,4,0] = ps_run['KineticEnergy_in']
+        ps_np[:,4,1] = ps_run['KineticEnergy_out']
+        ps_np[:,4,2] = ps_run['TrackID'] if args_info.no_nuclear else ps_run['TrackID_out']
 
-        df_itk = itk.GetImageFromArray(df_np, ttype=ImageType)
+        df_itk = itk.GetImageFromArray(ps_np, ttype=ImageType)
 
         output_file = args_info.output.replace('.', f'{r:04d}.')
         itk.imwrite(df_itk, output_file)
